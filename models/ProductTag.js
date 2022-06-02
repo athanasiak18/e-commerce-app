775444f4193b1cf3ca96468c87dbaeb4616ee2b3
Athanasia <athanasiakotopoulos@gmail.com>
2022-06-02 10:04:50 -0400

    prod tag

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,14 +6,28 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+        }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+
+        }
+    }
   },
   {
     sequelize,
     timestamps: false,
-    freezeTableName: true,
     underscored: true,
     modelName: 'product_tag',
+    freezeTableName: true
   }
 );
 
